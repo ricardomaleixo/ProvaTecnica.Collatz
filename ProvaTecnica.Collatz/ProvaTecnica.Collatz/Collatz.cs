383c9efacafb73606226d69ffe41d05b7765d42d
Ricardo Martins Aleixo <ricardoaleixoo@gmail.com>
2014-10-19 21:02:26 -0200

    Collatz

---
+++ ProvaTecnica.Collatz/ProvaTecnica.Collatz/Collatz.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ProvaTecnica.Collatz.Interface;
+using System.Linq;
+using System.Text;
+
+namespace ProvaTecnica.Collatz
+{
+    class Collatz : InterfaceCollatz
+    {
+        private const int limite = 100000;
+
+        public string exCollatz() {
+            try  {
+                    int inicio = 0;
+                    int maior = 0;
+                    int index;
+
+                for (int i = 2; i <= limite; i++) {
+                    int tamanho = 1;
+                    index = i;
+
+                    while (index != 1) {
+                        if ((index % 2) == 0 ) {
+                            index = index / 2;
+                        } else {
+                            index = index * 3 + 1;
+                            tamanho++;
+                        }
+                    }
+
+                    if (tamanho > inicio) {
+                        inicio = tamanho;
+                        maior = i;
+                    }
+                }
+
+            return string.Format("o que gera maior sequencia é:  {0}", maior);
+            }
+
+            catch(Exception e) {
+               return string.Concat("erro", e.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ ProvaTecnica.Collatz/ProvaTecnica.Collatz/Program.cs
@@ -0,0 +1,19 @@
+﻿using ProvaTecnica.Collatz.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaTecnica.Collatz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InterfaceCollatz exeCollazt = new Collatz();
+            Console.WriteLine(exeCollazt.exCollatz());
+            Console.ReadKey();
+        }
+    }
+}
